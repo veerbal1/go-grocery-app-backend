bf47182074e057c217273bdf0ff2d54a5ea10f9a
veerbal1 <veerbalsingh1@gmail.com>
2026-06-25 10:52:51 +0530

    Created basic connection to db, reading .env file variables

--- main.go
+++ main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Missing .env file: %v", err)
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if strings.TrimSpace(dbURL) == "" {
+		log.Fatal("Missing DATABASE_URL")
+	}
+
+	fmt.Println("Connecting to db")
+	pg, err := pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		log.Fatalf("Failed to make connection to db: %v", err)
+	}
+	fmt.Println("Connected to DB")
+
+	defer pg.Close()
+
+	fmt.Println("Sending ping to DB")
+	err = pg.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("Err: not able to ping database %v", err)
+	}
+	fmt.Println("Ping successful to DB")
+
+	fmt.Println("Building HTTP server...")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome Veerbal to golang backend journey")
+		fmt.Fprint(w, "Hello, welcome to golang")
 	})
 
-	fmt.Println("Server is listening on port :8080")
-	err := http.ListenAndServe(":8080", nil)
-
+	fmt.Println("Listening on port :8080")
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to listen on port :8080  - %v", err)
 	}
 }
